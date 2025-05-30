bdeed9a4935be8b7347e5474bc13d959c5ba8b56
Alexander Scherbakov <webmasterdracarys@gmail.com>
2025-05-30 16:15:17 +0300

    Побаловался с консольным вводом, записал кучу заметок по поводу обьявления переменных и классов

--- dotnet-learning/Program.cs
+++ dotnet-learning/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+namespace dotnet_learning
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var data = Console.ReadLine();
+            var sb = new System.Text.StringBuilder();
+            sb.Append("Hello World! Меня зовут ").Append(data);
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
