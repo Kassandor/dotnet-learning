ca442774b0d36c5ed748a1a98de2082a0a5820d6
Alexander Scherbakov <webmasterdracarys@gmail.com>
2025-06-02 17:51:47 +0300

    Рефактор структуры проекта

--- src/dotnet-learning/Program.cs
+++ src/dotnet-learning/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Text;
+
 namespace dotnet_learning
 {
     internal class Program
@@ -7,7 +9,7 @@ namespace dotnet_learning
         private static void Main(string[] args)
         {
             var data = Console.ReadLine();
-            var sb = new System.Text.StringBuilder();
+            var sb = new StringBuilder();
             sb.Append("Hello World! Меня зовут ").Append(data);
             Console.WriteLine(sb.ToString());
         }
